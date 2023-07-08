cb7caf1fbe405ab78eb77f714098ccbb0f6cc746
azizkhlify <110391579+azizkhlify@users.noreply.github.com>
2023-07-08 15:01:52 +0100

    Update Jos-Prob.py

--- Jos-Prob.py
+++ Jos-Prob.py
@@ -1,1 +1,19 @@
-print('hi') 
+def sous(c,n):
+    if n>=len(c)-1:return [c[1:],n-(len(c)-1)]
+    return [c[:n+1]+c[n+2:],n+1]
+def jos(c,n):
+    if len(c)==1:return c[0]
+    elif 0<=n<=len(c)-1 :
+        x=sous(c,n)
+        return jos(x[0],x[1])
+    else :
+        n%=(len(c))
+        return jos(c,n)
+def remplir(n):
+    c=[]
+    for i in range(n):
+        c.append(i+1)
+    return c
+n=int(input())
+t=remplir(n)
+print(jos(t,0))
